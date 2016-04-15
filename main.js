5aa42743b2827e37a909571f12129941dc109aee
epavlenko <epavlenko@devexperts.com>
2016-04-15 21:41:59 +0300

    Use vk execute

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+var APIKEY = 5377462;
+
+
 function auth() {
 	VK.init({
-		apiId: 5377462
+		apiId: APIKEY
 	});
 	return new Promise(function(resolve, reject) {
 			VK.Auth.login(function(response) {
@@ -16,37 +19,50 @@ function auth() {
 
 auth()
 	.then(function() {
-		return getAllFiends(4289506)
+		return getAllFiends(4289506);
 	})
 	.then(function(friendsList) {
-		var promises = friendsList.map(function(friend) {
-			return getAllFiends(friend)
+		var secondLevelMutual = [];
+		friendsList.sub.forEach(function(item) {
+			secondLevelMutual = secondLevelMutual.concat(item.mutualList);
 		});
-		Promise.all(promises)
-			.then(function(result) {
-				console.log(result)
-			});
+		secondLevelMutual = secondLevelMutual.filter(function(id, i) {
+			return id && !(secondLevelMutual.indexOf(id, i + 1) > i);
+		});
+		console.log('Mutual friends with this user ' + friendsList.self.length);
+		console.log('Mutual friends with this his friends ' + secondLevelMutual.length);
+
+
 	})
 	.catch(function(error) {
 		console.error(error)
 	});
 
+
 function getAllFiends(userId) {
+	var code =
+		'var a=API.friends.getMutual({"target_uid": "' + userId + '"});' +
+		'var len = a.length -1 ;' +
+		'var c = []; c.sub = [];' +
+		'c.self = API.friends.getMutual({"target_uid":"' + userId + '"});' +
+		'while(len>0){' +
+		'var friend = []; friend.id = a[len]; friend.mutualList = API.friends.getMutual({"target_uid":a[len]}); '+
+		'c.sub.push(friend);' +
+		'len = len - 1;}' +
+		'return c;';
+
 	return new Promise(function(resolve, reject) {
-		VK.api('friends.getMutual', {
-			'target_uid': userId
-		}, function(response) {
+		VK.Api.call("execute", {code: code}, callback);
+		function callback(response) {
 			if (response.error) {
-				if (response.error.error_code === 15) {
-					resolve([]);
-				} else {
-					reject(new Error(response.error.error_msg));
-				}
+				reject(new Error(response.error.error_msg));
 			} else {
 				resolve(response.response);
 			}
-
-		})
-	})
-
+		}
+	});
 }
+
+
+
+
